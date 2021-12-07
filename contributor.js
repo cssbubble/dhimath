bad499579f5bf9c3dba0192097a10995616a3922
cssbubble <cssbubble@gmail.com>
2021-12-07 06:02:36 +0000

    Add files via upload

---
+++ contributor.js
@@ -0,0 +1,51 @@
+var contriData = [
+    {
+        id: "1",
+        educator : "Priyanka",
+        special: "Early Maths",
+        profile_img: "https://www.pphfoundation.ca/wp-content/uploads/2018/05/default-avatar.png"
+    }, 
+    {
+        id: "2",
+        educator : "Kashmira",
+        special: "Economics",
+        profile_img: "https://randomuser.me/api/portraits/women/1.jpg"
+    }, 
+    {
+        id:"3",
+        educator:"Kiran",
+        special:"Exploding dots",
+        profile_img: "https://randomuser.me/api/portraits/men/1.jpg"
+    }, 
+    {
+        id:"4",
+        educator:"Nilesh",
+        special:"Advanced Maths",
+        profile_img: "https://randomuser.me/api/portraits/men/2.jpg"
+    }, 
+    {
+        id:"5",
+        educator:"VSS. Sastry",
+        special:"Origami",
+        profile_img: "https://randomuser.me/api/portraits/men/3.jpg"
+
+    }, 
+]
+
+var faqs = [
+    {
+        id: "1",
+        question: "What is our mission?",
+        answer: "To bring joy of mathematics to all.",
+    },
+    {
+        id: "2",
+        question: "Where is our office?",
+        answer: "In cloud, its virtual",
+    },
+    {
+        id: "3",
+        question: "What subject we cover?",
+        answer: "Recreational and game maths",
+    }
+]
